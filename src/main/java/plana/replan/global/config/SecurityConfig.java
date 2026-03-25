9fa8f20f465cd935b39dcae601a9131cd24f3c04
_yoonyoon_1 <heeyoon1302@naver.com>
2026-03-25 20:51:54 +0900

    fix : SecurityConfig Swagger 경로 추가

--- src/main/java/plana/replan/global/config/SecurityConfig.java
+++ src/main/java/plana/replan/global/config/SecurityConfig.java
@@ -16,7 +16,8 @@ public class SecurityConfig {
     http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html")
+                auth.requestMatchers(
+                        "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs", "/swagger-ui.html")
                     .permitAll()
                     .anyRequest()
                     .authenticated());
