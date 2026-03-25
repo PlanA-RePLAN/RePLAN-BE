06e7e7ada89b66dce780e2323374f1eb297e06ae
_yoonyoon_1 <heeyoon1302@naver.com>
2026-03-25 11:17:14 +0900

    chore: spotless 세팅

--- src/main/java/plana/replan/ReplanApplication.java
+++ src/main/java/plana/replan/ReplanApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReplanApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReplanApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ReplanApplication.class, args);
+  }
 }
