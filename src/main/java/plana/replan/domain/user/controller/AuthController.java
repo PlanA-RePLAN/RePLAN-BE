27781a8c28fb6d974764b30238a29fde9bd7f753
_yoonyoon_1 <heeyoon1302@naver.com>
2026-03-30 18:51:52 +0900

    Feat: 로그인 API 구현

--- src/main/java/plana/replan/domain/user/controller/AuthController.java
+++ src/main/java/plana/replan/domain/user/controller/AuthController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import plana.replan.domain.user.dto.LoginRequestDto;
+import plana.replan.domain.user.dto.LoginResponseDto;
 import plana.replan.domain.user.dto.SignUpRequestDto;
 import plana.replan.domain.user.service.AuthService;
 
@@ -21,5 +23,10 @@ public class AuthController {
   public ResponseEntity<Void> signUp(@Valid @RequestBody SignUpRequestDto request) {
     authService.signUp(request);
     return ResponseEntity.ok().build();
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto request) {
+    return ResponseEntity.ok(authService.login(request));
   }
 }
