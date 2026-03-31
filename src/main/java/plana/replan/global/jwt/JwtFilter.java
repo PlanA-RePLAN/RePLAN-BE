d720a5263e5c8451bd18b6ac082590920ab76bfd
_yoonyoon_1 <heeyoon1302@naver.com>
2026-03-31 13:54:56 +0900

    Fix: JwtFilter에서 auth 경로 필터링 제외

--- src/main/java/plana/replan/global/jwt/JwtFilter.java
+++ src/main/java/plana/replan/global/jwt/JwtFilter.java
@@ -19,6 +19,12 @@ public class JwtFilter extends OncePerRequestFilter {
   private final JwtUtil jwtUtil;
 
   @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) {
+    String path = request.getRequestURI();
+    return path.startsWith("/api/auth/");
+  }
+
+  @Override
   protected void doFilterInternal(
       HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
