4b968b6f87a957a5ac0beb5d08655d13cf55d5d0
seojimin <jnuni1004@ewhain.net>
2026-05-09 13:24:26 +0900

    fix/#79-modifySchema

--- src/main/java/plana/replan/global/entity/BaseTimeEntity.java
+++ src/main/java/plana/replan/global/entity/BaseTimeEntity.java
@@ -4,11 +4,16 @@ import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @MappedSuperclass
 @Getter
 public abstract class BaseTimeEntity {
+
+  @CreationTimestamp
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
 
   @UpdateTimestamp
   @Column(name = "updated_at")
