c7017c5d68a3eb9a1d3d787d2791204d2eb18f44
seojimin <jnuni1004@ewhain.net>
2026-05-09 13:38:05 +0900

    fix/#79-modifySchema

--- src/main/resources/db/migration/V2__add_created_at_and_set_is_pinned_default.sql
+++ src/main/resources/db/migration/V2__add_created_at_and_set_is_pinned_default.sql
@@ -1,9 +1,26 @@
 ALTER TABLE users ADD COLUMN created_at TIMESTAMP;
++UPDATE users SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE users SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
++
 ALTER TABLE goal ADD COLUMN created_at TIMESTAMP;
++UPDATE goal SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE goal SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
++
 ALTER TABLE tag ADD COLUMN created_at TIMESTAMP;
++UPDATE tag SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE tag SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
++
 ALTER TABLE routine ADD COLUMN created_at TIMESTAMP;
++UPDATE routine SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE routine SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
++
 ALTER TABLE todo ADD COLUMN created_at TIMESTAMP;
++UPDATE todo SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE todo SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
++
 ALTER TABLE failure_reason ADD COLUMN created_at TIMESTAMP;
++UPDATE failure_reason SET created_at = updated_at WHERE created_at IS NULL AND updated_at IS NOT NULL;
++UPDATE failure_reason SET created_at = CURRENT_TIMESTAMP WHERE created_at IS NULL;
 
 ALTER TABLE todo ALTER COLUMN is_pinned SET DEFAULT false;
 UPDATE todo SET is_pinned = false WHERE is_pinned IS NULL;
