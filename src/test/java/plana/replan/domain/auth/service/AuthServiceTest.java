5569889eaa5c36d7e0ba1b5001bea64fbdcd40f9
heyoonyoon <heeyoon1302@naver.com>
2026-06-27 21:50:04 +0900

    Refactor: 애플 로그인 테스트 군더더기 파일/임포트 정리

--- src/test/java/plana/replan/domain/auth/service/AuthServiceTest.java
+++
@@ -1,5 +0,0 @@
-package plana.replan.domain.auth.service;
-
-// AuthService 통합 테스트는 기능별로 분리된 테스트 클래스에서 관리합니다.
-// AuthServiceGoogleLoginTest, AuthServiceKakaoLoginTest, AuthServiceAppleLoginTest 등 참고.
-class AuthServiceTest {}
